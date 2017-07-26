67421cb59e89e1c175dc4ee2dfd26e6b7870815a
Lucas Almeida <lucasrca@protonmail.com>
2017-07-26 20:28:03 -0300

    Editar blog/post

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.template import loader
 
-# Create your views here.
+from .models import Post
+
+# CMS - Views
+
+# Lista os posts
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'cms/post_list.html', {})
+
